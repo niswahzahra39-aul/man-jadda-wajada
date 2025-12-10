dac5241651801b5a8940b64410a637a031a42011
niswah aulia <niswahzahra39@gmail.com>
2025-12-11 00:26:55 +0700

    mengubah data

--- pages/page2.py
+++ pages/page2.py
@@ -5,4 +5,13 @@ import matplotlib.pyplot as plt
 
 st.title("Data Visualization")
 
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+
+fig, ax = plt.subplots()
+ax.plot(x, y)
+
+st.pyplot(fig)
+
+
 df = pd.read_csv("data/DaftarSaham.csv")
