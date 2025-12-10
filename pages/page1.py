f0a47a311a7aedb4bcb1712030f0cb6937260cb5
niswah aulia <niswahzahra39@gmail.com>
2025-12-10 23:52:16 +0700

    mengubah judul

--- pages/page1.py
+++ pages/page1.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
 st.title("Analisis Performa Saham Perusahaan Terdaftar di Bursa Efek Indonesia")
+st.header("Tugas Kelompok Man-Jadda-Wajada")
 st.write("This is main landing page of the app.")
 st.write("This is main landing page of the app.")
