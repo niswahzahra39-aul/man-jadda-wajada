9dcc13390a2f0ec1f50472deca332927bd2d1f12
niswah aulia <niswahzahra39@gmail.com>
2025-12-11 00:43:08 +0700

    menambahkan data

--- pages/page2.py
+++ pages/page2.py
@@ -14,6 +14,6 @@ ax.plot(x, y)
 st.pyplot(fig)
 
 
-if uploaded:
-    df = pd.read_csv("data/DaftarSaham.csv")
-    st.write(df.head())
+df = pd.read_csv("data/DaftarSaham.csv")
+
+st.write(df.head())
