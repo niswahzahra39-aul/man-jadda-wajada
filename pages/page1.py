e899bd0eccf3889e5dc458c71627910a01b34448
niswah aulia <niswahzahra39@gmail.com>
2025-12-12 11:11:34 +0700

    mengedit file

--- pages/page1.py
+++ pages/page1.py
@@ -18,14 +18,6 @@ import pandas as pd
 
 st.title("Data Visualization")
 
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-
-fig, ax = plt.subplots()
-ax.plot(x, y)
-
-st.pyplot(fig)
-
 
 df = pd.read_csv("data/DaftarSaham.csv")  
 
