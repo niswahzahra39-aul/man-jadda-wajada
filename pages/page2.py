6fd03fa5b63e52e2d47cd0224a27f7086a8c2c4c
niswah aulia <niswahzahra39@gmail.com>
2025-12-17 20:44:46 +0700

    mengedit file page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -11,3 +11,13 @@ df = pd.read_csv("data/DaftarSaham.csv")
 
 st.write(df.head())
 st.write(df.tail())
+
+marketcap_sector = df.groupby('Sector')['MarketCap'].sum().sort_values(ascending=False)
+
+plt.figure()
+marketcap_sector.plot(kind='bar')
+plt.xlabel("Sektor Industri")
+plt.ylabel("Total Kapitalisasi Pasar")
+plt.title("Kapitalisasi Pasar Saham Berdasarkan Sektor di Bursa Efek Indonesia")
+
+st.pyplot(plt)
