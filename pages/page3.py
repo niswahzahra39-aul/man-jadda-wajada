49d64cf9834bf0e2772050e30b346e3032897166
niswah aulia <niswahzahra39@gmail.com>
2025-12-17 21:50:42 +0700

    mengedit file page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -3,3 +3,11 @@ import streamlit as st
 st.subheader("Analisis dari grafik data")
 st.write("Berdasarkan grafik kapitalisasi pasar di setiap sektor industri, terlihat bahwa nilai kapitalisasi pasar saham di Bursa Efek Indonesia tidak sama antar sektor. Beberapa sektor memiliki kapitalisasi pasar yang lebih besar dibandingkan sektor lainnya, yang menunjukkan bahwa sektor tersebut berperan lebih besar dalam struktur pasar saham Indonesia. Sementara itu, sektor dengan kapitalisasi pasar lebih kecil menunjukkan perbedaan ukuran dan ciri perusahaan yang beroperasi di masing-masing sektor. Selain itu, grafik yang menampilkan proporsi kapitalisasi pasar per sektor menunjukkan bahwa nilai pasar saham di Bursa Efek Indonesia lebih banyak terpusat pada sektor-sektor tertentu. Hal ini menunjukkan bahwa pergerakan pasar saham secara keseluruhan dipengaruhi oleh kinerja sektor-sektor utama tersebut. Dengan demikian, analisis kapitalisasi pasar per sektor memberikan gambaran yang penting mengenai struktur dan penyebaran nilai pasar saham di Bursa Efek Indonesia.")
 
+st.subheader("Kesimpulan")
+
+st.write("Berdasarkan hasil analisis yang dilakukan, dapat disimpulkan bahwa nilai kapitalisasi pasar saham di Bursa Efek Indonesia tidak merata di setiap sektor industri. Beberapa sektor memiliki nilai kapitalisasi pasar yang lebih besar dan cenderung menguasai pasar saham, sedangkan sektor lainnya memiliki kontribusi yang lebih kecil. Hal ini menunjukkan adanya perbedaan dalam skala dan nilai pasar perusahaan di setiap sektor. Oleh karena itu, sektor industri menjadi faktor penting dalam memahami struktur dan bagaimana nilai kapitalisasi pasar terdistribusi di Bursa Efek Indonesia, sehingga bisa menjadi pertimbangan bagi investor dalam memahami kondisi pasar saham secara menyeluruh.")
+
+
+st.header("Daftar Pustaka")
+st.write("https://www.kaggle.com/datasets/muamkh/ihsgstockdata")
+
